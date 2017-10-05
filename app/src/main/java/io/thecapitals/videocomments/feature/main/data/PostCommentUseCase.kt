79b31a81bb5968225aa8eba548044b71c750f68c
Mateusz Bratkowski <mateusz.bratkowski93@gmail.com>
2017-10-05 14:47:34 +0200

    Implemented first usecase to send data

--- app/src/main/java/io/thecapitals/videocomments/feature/main/data/PostCommentUseCase.kt
+++ app/src/main/java/io/thecapitals/videocomments/feature/main/data/PostCommentUseCase.kt
@@ -1,7 +1,19 @@
 package io.thecapitals.videocomments.feature.main.data
 
+import android.util.Log
+import com.google.firebase.firestore.FirebaseFirestore
+import io.thecapitals.videocomments.feature.main.model.MessageModel
+
 /**
  * Created for project VideoComments on 05/10/2017.
  */
-class PostCommentUseCase {
+class PostCommentUseCase(val firebaseFirestore: FirebaseFirestore) : BaseUseCase {
+
+    fun sendMessage(message: MessageModel) {
+        firebaseFirestore.collection("users")
+                .add(message.data)
+                .addOnSuccessListener { documentReference -> Log.d("Usecase", "DocumentSnapshot added with ID: " + documentReference.id) }
+                .addOnFailureListener { error -> Log.w("Usecase", "Error adding document", error) }
+    }
+
 }
